6d6e1dbeac834dc3d87eb213408706c5b3debd38
Thexisis <142893901+Thexisis@users.noreply.github.com>
2024-08-09 04:22:02 +0000

    Developer name change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-12 w-full flex justify-between gap-4 text-sm py-2 max-sm:flex-col">
-      <p className="text-green-1">Developer: Travis( final year roject KTU)</p>
+      <p className="text-green-1">Developer: Isaac Travis Yanney(final year project KTU)</p>
       <p className="text-sky-1">© 2024 - All rights reserved by KTU</p>
     </footer>
   );
